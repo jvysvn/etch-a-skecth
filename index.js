1c037e630da3944993d79546e6e7bb38745dbcdc
jvysvn <jaysonptbull@gmail.com>
2024-01-13 12:11:42 -0500

    added click to draw

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let color = 'black';
+let click = false;
 
 document.addEventListener('DOMContentLoaded', function () {
   createBoard(16);
+
+  document.querySelector('body').addEventListener('click', function (e) {
+    if (e.target.tagName != 'BUTTON') {
+      click = !click;
+      let draw = document.querySelector('#draw');
+      if (click) {
+        draw.innerHTML = 'Drawing on';
+      } else {
+        draw.innerHTML = 'Drawing off';
+      }
+    }
+  });
 
   let btn_popup = document.querySelector('#popup');
   btn_popup.addEventListener('click', function () {
@@ -39,10 +52,12 @@ function getSize() {
 }
 
 function colorDiv() {
-  if (color == 'random') {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  } else {
-    this.style.backgroundColor = 'black';
+  if (click) {
+    if (color == 'random') {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = 'black';
+    }
   }
 }
 
